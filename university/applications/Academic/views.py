4e1b362ad12a86d7f9bbf77b1facb3d7bc6a52c9
jemosdev <jerson.mosquera.p@gmail.com>
2023-05-27 22:12:38 -0500

    template and view function updated

--- university/applications/Academic/views.py
+++ university/applications/Academic/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Curso
 
 # Create your views here.
 def home(request):
-    return render(request, "gestionCursos.html")
+    cursosListados = Curso.objects.all()
+    return render(request, "gestionCursos.html", {"cursos": cursosListados})
