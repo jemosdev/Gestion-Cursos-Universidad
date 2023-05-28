be9373cce3114d51f738f9a9dee062fdedc7d408
jemosdev <jerson.mosquera.p@gmail.com>
2023-05-28 00:35:12 -0500

    creating form and post method

--- university/applications/Academic/views.py
+++ university/applications/Academic/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Curso
 
 # Create your views here.
 def home(request):
     cursosListados = Curso.objects.all()
     return render(request, "gestionCursos.html", {"cursos": cursosListados})
+
+def registrarCurso(request):
+    codigo = request.POST['txtCodigo']
+    nombre = request.POST['txtNombre']
+    creditos = request.POST['numCreditos']
+    
+    curso = Curso.objects.create(codigo= codigo, nombre= nombre, creditos= creditos)
+    return redirect('/')
