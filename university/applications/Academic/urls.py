698bd6f762e3d5600607779454d91bfed8482d36
jemosdev <jerson.mosquera.p@gmail.com>
2023-05-28 01:08:53 -0500

    creating edition method

--- university/applications/Academic/urls.py
+++ university/applications/Academic/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('registrarCurso/', views.registrarCurso),
+    path('edicionCurso/<codigo>', views.edicionCurso),
+    path('editarCurso/', views.editarCurso),
     path('eliminarCurso/<codigo>', views.eliminarCurso)
 ]
--- university/applications/Academic/views.py
+++ university/applications/Academic/views.py
@@ -14,6 +14,22 @@ def registrarCurso(request):
     curso = Curso.objects.create(codigo= codigo, nombre= nombre, creditos= creditos)
     return redirect('/')
 
+def edicionCurso(request, codigo):
+    cursoEditar= Curso.objects.get(codigo= codigo)
+    return render(request, "edicionCurso.html", {"curso": cursoEditar})
+
+def editarCurso(request):
+    codigo = request.POST['txtCodigo']
+    nombre = request.POST['txtNombre']
+    creditos = request.POST['numCreditos']
+
+    curso= Curso.objects.get(codigo= codigo)
+    curso.nombre= nombre
+    curso.creditos= creditos
+    curso.save()
+    
+    return redirect('/')
+
 def eliminarCurso(request, codigo):
     curso= Curso.objects.get(codigo= codigo)
     curso.delete()
