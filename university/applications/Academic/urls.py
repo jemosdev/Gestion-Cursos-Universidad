c62811c986678a3c8ad4e727d66bffbeb1d704ec
jemosdev <jerson.mosquera.p@gmail.com>
2023-05-28 00:41:58 -0500

    create delete registers

--- university/applications/Academic/urls.py
+++ university/applications/Academic/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('registrarCurso/', views.registrarCurso)
+    path('registrarCurso/', views.registrarCurso),
+    path('eliminarCurso/<codigo>', views.eliminarCurso)
 ]
