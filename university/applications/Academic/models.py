18c5142f118b1d5c85248dcd5802faef723aa997
jemosdev <jerson.mosquera.p@gmail.com>
2023-05-27 21:39:42 -0500

    updating model and admin with view

--- university/applications/Academic/models.py
+++ university/applications/Academic/models.py
@@ -6,3 +6,8 @@ class Curso(models.Model):
     codigo= models.CharField(primary_key=True, max_length= 10)
     nombre= models.CharField(max_length=50)
     creditos= models.PositiveSmallIntegerField()
+
+    #how to show the information in admin console
+    def __str__(self):
+        texto = "{0} ({1})"
+        return texto.format(self.nombre, self.creditos)
