51403735954702a4317629cf60d7936d62f77fa4
jemosdev <jerson.mosquera.p@gmail.com>
2023-05-27 21:24:16 -0500

    creating model and admin console

--- university/applications/Academic/models.py
+++ university/applications/Academic/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Curso(models.Model):
+    codigo= models.CharField(primary_key=True, max_length= 10)
+    nombre= models.CharField(max_length=50)
+    creditos= models.PositiveSmallIntegerField()
